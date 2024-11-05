68f194fb81100022bd22afa4acc1b4782638412e
Evgeny Seliverstov <ifau@me.com>
2024-11-05 14:23:58 +0300

    Initial Commit

---
+++ Drinkie/DrinkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkieApp.swift
+//  Drinkie
+//
+//  Created by ifau on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrinkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
